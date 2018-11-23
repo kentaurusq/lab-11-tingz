80beaf52f5e7d207abe39c717881509a8807b1b6
kentaurusq <43187749+kentaurusq@users.noreply.github.com>
2018-11-23 17:05:59 -0500

    Add files via upload

---
+++ Lab11/ex1scripts.js
@@ -0,0 +1,62 @@
+var colorPicker = ["red", "green", "blue", "yellow"];
+var otherArray = new Array(); 
+
+function startGame() {
+	var randoNum = Math.floor(Math.random() * 4);
+	otherArray.unshift(colorPicker[randoNum]);
+	document.getElementById("simonSays").innerHTML = "Simon Says:" + colorPicker[randoNum];
+	setTimeout(changeCol, 500);
+	
+	}
+	
+function changeCol() {
+	document.getElementById("simonSays").innerHTML = "Simon Says:";
+}
+
+function redButt() {
+	
+	var red = "red";
+	if (red == otherArray[0]){
+	startGame();
+	}
+	else {
+	document.getElementById("simonSays").innerHTML = "Simon Says: " + otherArray;
+	}
+}
+
+function greenButt() {
+	
+	var green = "green";
+	if (green == otherArray[0]){
+	startGame();
+	}
+	else {
+	document.getElementById("simonSays").innerHTML = "Simon Says: " + otherArray;
+	}
+}
+
+function blueButt() {
+	
+	var blue = "blue";
+	if (blue == otherArray[0]){
+	startGame();
+	}
+	else {
+	document.getElementById("simonSays").innerHTML = "Simon Says: " + otherArray;
+	}
+}
+
+function yellowButt() {
+	
+	var yellow = "yellow";
+	if (yellow == otherArray[0]){
+	startGame();
+	}
+	else {
+	document.getElementById("simonSays").innerHTML = "Simon Says: " + otherArray;
+	}
+}
+
+
+	
+	
